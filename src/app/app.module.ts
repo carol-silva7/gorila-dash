4c7a5a7e96f389b3459ac7c7507fa08bc0514155
Carol-silva7 <silva.c@aluno.ufabc.edu.br>
2021-02-02 01:38:27 -0300

    criação das pastas pages e components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ActiveComponent } from './pages/active/active.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ActiveComponent
   ],
   imports: [
     BrowserModule,
